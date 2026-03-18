9e3607bff0c53dcb51a33b76e689c77806828de7
稲永翔 <shoma@def.local>
2026-03-18 12:26:01 +0900

    投稿の編集機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :posts, only:[ :index, :new, :create ]
+  resources :posts, only:[ :index, :new, :create, :edit, :update, :destroy ]
   root "posts#index"
 
 end
