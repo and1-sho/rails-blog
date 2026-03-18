29dcc9392c9051677b244f3d7e5e92980c8eae9d
稲永翔 <shoma@def.local>
2026-03-18 11:10:44 +0900

    投稿の新規作成機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,29 @@
 class PostsController < ApplicationController
+  # 一覧
   def index
     @posts = Post.all
+  end
+
+  #　新規投稿 
+  def new
+    @post = Post.new
+  end
+
+  # 投稿処理
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path 
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :posts, only:[ :index ]
+  resources :posts, only:[ :index, :new, :create ]
   root "posts#index"
 
 end
